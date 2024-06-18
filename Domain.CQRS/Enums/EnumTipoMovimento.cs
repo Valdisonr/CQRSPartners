8cb037669b6f3c27ceaf159e77488d152fd0e987
Valdison Metelis <valdison@outlook.com>
2024-06-17 22:57:20 -0300

    Adicionar arquivos de projeto.

---
+++ Domain.CQRS/Enums/EnumTipoMovimento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.CQRS.Enums
+{
+    public enum  EnumTipoMovimento
+    {
+        Entrada=1,
+        Saida=2,
+        Ajuste=3
+    }
+}
