568e12a13639db93ea53ddd20ff3c434759a1e02
Valdison Metelis <valdison@outlook.com>
2024-06-18 22:57:58 -0300

    add02

---
+++ Application.CQRS/DepencyInjection/DependecyInjection.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.DepencyInjection
+{
+public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+                 this IServiceCollection services,
+                 IConfiguration configuration)
+        {
+            var mySqlConnection = configuration
+                                  .GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ContextoDB>(options =>
+                             options.UseMySql(mySqlConnection,
+                             ServerVersion.AutoDetect(mySqlConnection)));
+
+            // Registrar IDbConnection como uma instância única
+            services.AddSingleton<IDbConnection>(provider =>
+            {
+                var connection = new MySqlConnection(mySqlConnection);
+                connection.Open();
+                return connection;
+            });
+
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IMemberDapperRepository, MemberDapperRepository>();
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArch.Application");
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(myhandlers);
+                cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+            });
+
+            services.AddValidatorsFromAssembly(Assembly.Load("CleanArch.Application"));
+
+            return services;
+        }
+    }
+}
--- Infra.CQRS/Repositories/ProdutoRepository.cs
+++ Infra.CQRS/Repositories/ProdutoRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.CQRS.Entities;
+using Domain.CQRS.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,9 @@ using System.Threading.Tasks;
 
 namespace Infra.Data.CQRS.Repositories
 {
-    public class ProdutoRepository
-    {
+    public class ProdutoRepository:RepositoryGeneric<Produto>,IProdutoRepository 
+    { 
+
 
     }
 }
