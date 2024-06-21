3ac6af3985d9259e0be9808ba7747f36037b8133
Valdisonr <valdison@outlook.com>
2024-06-20 23:56:52 -0400

    add2006

---
+++ Application.CQRS/ProdutosCases/ProdutosCommands/ProdutoCreateCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.CQRS.Produtos.ProdutosCommands.ProdutoNotifications;
+using Domain.CQRS.Entities;
+using Domain.CQRS.Interfaces;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.ProdutosCases.ProdutosCommands
+{
+    public class  ProdutoCreateCommand : ProdutoCommandBase
+    {
+
+
+        public class ProdutoCreateCommandHanler : IRequestHandler<ProdutoCreateCommand, Produto>
+        {
+
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IValidator<ProdutoCreateCommand> _validator;
+            private readonly IMediator _mediator;
+
+            public async Task<Produto> Handle(ProdutoCreateCommand request, CancellationToken cancellationToken)
+            {
+               
+                _validator.ValidateAndThrow(request);
+                var newProduto=new Produto(request.Nome,request.Descricao,request.CodigoBarra,request.DataCriacao,request.DataAtualizacao);
+                await _unitOfWork.ProdutoRepository.Adicionar(newProduto);
+                await _unitOfWork.CommitAsync();
+                await _mediator.Publish(new ProdutoCreatedNotification(newProduto), cancellationToken);
+                return newProduto;
+              
+            }
+        }
+
+    }
+}
--- Application.CQRS/Validations/ProdutoCreateCommandValidator.cs
+++ Application.CQRS/Validations/ProdutoCreateCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Application.CQRS.Validations
 {
-    internal class ProdutoCreateCommand
+    public class ProdutoCreateCommandValidator:AbstractValidator<produ>
     {
+
     }
 }
